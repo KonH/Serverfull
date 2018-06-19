9cdf521219df6022ec9bbffca84a4b27b408db73
konh <konh@yandex.ru>
2018-06-19 18:13:36 +0700

    Import UDBase & setup project;

---
+++ Assets/Scripts/Installers/CommonInstaller.cs
@@ -0,0 +1,11 @@
+﻿using UDBase.Installers;
+using UDBase.Controllers.LogSystem;
+
+public class CommonInstaller : UDBaseInstaller {
+	public UnityLog.Settings LogSettings;
+
+	public override void InstallBindings() {
+		AddEvents();
+		AddUnityLogger(LogSettings);
+	}
+}
