83af7061f7cf3fed58497e7913e10c04d311e33a
konh <konh@yandex.ru>
2018-07-19 20:38:33 +0700

    DI container usage improvements;

--- Assets/Scripts/Installers/GameInstaller.cs
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using UDBase.Installers;
+﻿using UDBase.Installers;
 using Serverfull.Game;
+using Serverfull.Utils;
 using Serverfull.Controllers;
-using Zenject;
 
 namespace Serverfull.Installers {
 	public class GameInstaller : UDBaseInstaller {
 		public GameSettings  Settings;
 		public ServerManager ServerManager;
 		public ServerBuilder ServerBuilder;
-
+		
 		public override void InstallBindings() {
 			Container.BindInstance(Settings);
 			Container.Bind<GameRules>().ToSelf().AsSingle();
-			Container.Bind(typeof(TimeController), typeof(ITickable)).To<TimeController>().AsSingle();
+			Container.BindTickableToSelf<TimeController>().AsSingle();
 			Container.Bind<ClientController>().ToSelf().AsSingle();
 			Container.Bind<UserController>().ToSelf().AsSingle();
-			Container.Bind(typeof(RequestController), typeof(ITickable)).To<RequestController>().AsSingle();
-			Container.Bind(typeof(RequestSpawnController), typeof(ITickable)).To<RequestSpawnController>().AsSingle().NonLazy();
-			Container.Bind(typeof(TransportController), typeof(IInitializable), typeof(IDisposable)).To<TransportController>().AsSingle();
-			Container.Bind(typeof(ProcessingController), typeof(IInitializable), typeof(IDisposable)).To<ProcessingController>().AsSingle();
+			Container.BindTickableToSelf<RequestController>().AsSingle();
+			Container.BindTickableToSelf<RequestSpawnController>().AsSingle().NonLazy();
+			Container.BindInitDisposeToSelf<TransportController>().AsSingle();
+			Container.BindInitDisposeToSelf<ProcessingController>().AsSingle();
 			Container.Bind<ServerController>().ToSelf().AsSingle();
 			Container.Bind<ServerBuildController>().ToSelf().AsSingle().NonLazy();
-			Container.Bind(typeof(FinanceController), typeof(IInitializable), typeof(IDisposable)).To<FinanceController>().AsSingle();
+			Container.BindInitDisposeToSelf<FinanceController>().AsSingle();
 			Container.Bind<UpgradeController>().ToSelf().AsSingle();
-			Container.Bind(typeof(StatusController), typeof(IInitializable), typeof(IDisposable)).To<StatusController>().AsSingle();
-			Container.Bind(typeof(MessageController), typeof(IInitializable), typeof(IDisposable)).To<MessageController>().AsSingle().NonLazy();
+			Container.BindInitDisposeToSelf<StatusController>().AsSingle();
+			Container.BindInitDisposeToSelf<MessageController>().AsSingle().NonLazy();
 			Container.BindInstance(ServerManager);
 			Container.BindInstance(ServerBuilder);
 		}
---
+++ Assets/Scripts/Utils/DiContainerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Zenject;
+
+namespace Serverfull.Utils {
+	static class DiContainerExtensions {
+		public static ConcreteIdBinderNonGeneric BindTickable<T>(this DiContainer container) {
+			return container.Bind(typeof(T), typeof(ITickable));
+		}
+
+		public static FromBinderNonGeneric BindTickableToSelf<T>(this DiContainer container) {
+			return container.BindTickable<T>().To<T>();
+		}
+
+		public static ConcreteIdBinderNonGeneric BindInitDispose<T>(this DiContainer container) {
+			return container.Bind(typeof(T), typeof(IInitializable), typeof(IDisposable));
+		}
+
+		public static FromBinderNonGeneric BindInitDisposeToSelf<T>(this DiContainer container) {
+			return container.BindInitDispose<T>().To<T>();
+		}
+	}
+}
