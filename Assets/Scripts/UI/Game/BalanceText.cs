3ef8c9797ecb0f81340372761e6fdf5cc4957b70
konh <konh@yandex.ru>
2018-07-27 20:33:27 +0700

    Fix first time balance update issue;

--- Assets/Scripts/UI/Game/BalanceText.cs
+++ Assets/Scripts/UI/Game/BalanceText.cs
@@ -2,17 +2,22 @@
 using UnityEngine.UI;
 using UDBase.Controllers.EventSystem;
 using Serverfull.Events;
+using Serverfull.Models;
+using Serverfull.Controllers;
 using Zenject;
 
 namespace Serverfull.UI.Game {
 	[RequireComponent(typeof(Text))]
 	public class BalanceText : MonoBehaviour {
-		IEvent _event;
+		IEvent            _event;
+		FinanceController _finance;
+
 		Text   _text;
 
 		[Inject]
-		public void Init(IEvent events) {
-			_event = events;
+		public void Init(IEvent events, FinanceController finance) {
+			_event   = events;
+			_finance = finance;
 		}
 
 		void OnEnable() {
@@ -25,11 +30,16 @@ namespace Serverfull.UI.Game {
 
 		void Start() {
 			_text = GetComponent<Text>();
+			UpdateBalance(_finance.Balance);
 		}
 
 		void OnBalanceChanged(Balance_Changed e) {
+			UpdateBalance(e.NewBalance);
+		}
+
+		void UpdateBalance(Money value) {
 			if ( _text ) {
-				_text.text = e.NewBalance.ToString();
+				_text.text = value.ToString();
 			}
 		}
 	}
