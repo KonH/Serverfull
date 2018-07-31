cced68cea3d6e8b856dad2a9058da1b809785a10
konh <konh@yandex.ru>
2018-07-31 19:07:13 +0700

    UI tabs component;

---
+++ Assets/Scripts/UI/Common/TabsView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Serverfull.UI.Common {
+	public class TabsView : MonoBehaviour {
+		[Serializable]
+		public class TabSetup {
+			public Button     Header;
+			public GameObject Content;
+		}
+
+		public List<TabSetup> Tabs;
+
+		void Awake() {
+			InitTabs();
+			OpenTab(0);
+		}
+
+		void InitTabs() {
+			for ( var i = 0; i < Tabs.Count; i++ ) {
+				var index = i;
+				Tabs[i].Header.onClick.AddListener(() => OpenTab(index));
+			}
+		}
+
+		void OpenTab(int index) {
+			for ( var i = 0; i < Tabs.Count; i++ ) {
+				var isCurrentTab = (i == index);
+				Tabs[i].Header.interactable = !isCurrentTab;
+				Tabs[i].Content.SetActive(isCurrentTab);
+			}
+		}
+	}
+}
