f18eaacb57dcbf437246eec4ad87d0ed7f0d58b7
konh <konh@yandex.ru>
2018-09-04 20:15:19 +0700

    Add Menu scene;

---
+++ Assets/Scripts/Installers/MenuInstaller.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UDBase.Installers;
+namespace Serverfull.Installers {
+	public class MenuInstaller : UDBaseInstaller {
+
+		public override void InstallBindings() {
+			AddDirectSceneLoader();
+		}
+	}
+}
