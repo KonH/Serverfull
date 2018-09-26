1ce8f22c15fb0ccab3fd4b8c9332fbbcdd36183f
konh <konh@yandex.ru>
2018-09-26 20:18:06 +0700

    Adds temporary save button;

---
+++ Assets/Scripts/UI/Game/SaveGameButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Serverfull.Controllers;
+using Zenject;
+
+namespace Serverfull.UI.Game {
+	[RequireComponent(typeof(Button))]
+	public class SaveGameButton : MonoBehaviour {
+		SaveController _save;
+
+		[Inject]
+		void Init(SaveController save) {
+			_save = save;
+			GetComponent<Button>().onClick.AddListener(Save);
+		}
+
+		void Save() {
+			_save.Save();
+		}
+	}
+}
